5f8bda744c10e869acedc19fb0637abfb791d344
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-07-27 15:53:23 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,56 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
 import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs' 
+import {StyleSheet,Text,View,Image} from 'react-native';
 import WriteStoryScreen from './screens/WriteStoryScreen'
 import ReadStoryScreen from './screens/ReadStoryScreen'
+import LoginScreen from './screens/LoginScreen';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {createAppContainer,createSwitchNavigator} from 'react-navigation'
 
-export default class App extends React.Component {
-  render(){
-    return(
-      <AppContainer />
-    );
-  }
+export default function App() {
+  return (
+    <View>
+            <AppContainer/> 
+      
+
+    </View>
+  );
+  
 }
-
-const TabNavigator = createBottomTabNavigator({
-  WriteStory: WriteStoryScreen,
-  ReadStory: ReadStoryScreen
+const TabNavigator=createBottomTabNavigator({
+  Read: {screen:ReadStoryScreen},
+  Write: {screen: WriteStoryScreen}
 },
 {
-  defaultNavigationOptions: ({navigation})=>({
-    tabBarIcon: ()=>{
-      const routeName = navigation.state.routeName;
-      console.log(routeName)
-      if(routeName === "WriteStory"){
+  defaultNavigationOptions:({navigation})=>({
+    tabBarIcon:({})=>{
+      const routeName=navigation.state.routeName
+      if(routeName==='Read'){
         return(
-          <Image
-          source={require("./assets/write.png")}
-          style={{width:40, height:40}}
-        />
+          <Image source={require('./assets/read.png')}
+          style={{width: 40, height: 40}}/>
         )
-        
       }
-      else if(routeName === "ReadStory"){
+      else if(routeName==='Write'){
         return(
-          <Image
-          source={require("./assets/read.png")}
-          style={{width:40, height:40}}
-        />)
-        
+          <Image source={require('./assets/write.png')}
+          style={{width:40, height: 40}}/>
+        )
       }
     }
   })
-}
-  )
+})
+const SwitchNavigator = createSwitchNavigator({
 
-const AppContainer = createAppContainer(TabNavigator);
+  LoginScreen:{screen:LoginScreen},
+  TabNavigator:{screen:TabNavigator}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+})
+const AppContainer=createAppContainer(SwitchNavigator)
+
+
